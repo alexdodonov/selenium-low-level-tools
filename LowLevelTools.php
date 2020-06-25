d28b1837905261bb26c269225b4ecfd08bdb5215
gdever <alexey@dodonov.pro>
2020-06-25 17:31:38 +0300

    feature/Initial commit

---
+++ LowLevelTools.php
@@ -0,0 +1,278 @@
+<?php
+namespace Enterprize\Selenium;
+
+/**
+ * Selenium low level utilities wich are using only selenium bindings
+ *
+ * @author Dodonov A.A.
+ */
+class LowLevelTools extends \PHPUnit\Framework\TestCase
+{
+
+    /**
+     * Selenium driver
+     */
+    var $Driver = false;
+
+    /**
+     * Setting up test vars
+     */
+    public function setUp(): void
+    {
+        $Host = 'http://localhost:4444/wd/hub';
+
+        $Options = new \Facebook\WebDriver\Chrome\ChromeOptions();
+
+        global $argv;
+
+        if (in_array('iphonex', $argv)) {
+            $Options->addArguments([
+                '--window-size=375,812'
+            ]);
+        } elseif (in_array('ipad', $argv)) {
+            $Options->addArguments([
+                '--window-size=768,1024'
+            ]);
+        } else {
+            $Options->addArguments([
+                '--window-size=1400,800'
+            ]);
+        }
+
+        if (in_array('headless', $argv)) {
+            $Options->addArguments([
+                '--headless'
+            ]);
+        }
+
+        $Options->addArguments(
+            [
+                '--user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+            ]);
+
+        $capabilities = \Facebook\WebDriver\Remote\DesiredCapabilities::chrome();
+        $capabilities->setCapability(\Facebook\WebDriver\Chrome\ChromeOptions::CAPABILITY, $Options);
+        $this->Driver = \Facebook\WebDriver\Remote\RemoteWebDriver::create($Host, $capabilities, 5000);
+    }
+
+    /**
+     * Method destroys test variables
+     */
+    public function tearDown(): void
+    {
+        $Handles = $this->Driver->getWindowHandles();
+
+        foreach ($Handles as $Handle) {
+            $this->Driver->switchTo()->window($Handle);
+            $this->Driver->close();
+        }
+    }
+
+    /**
+     * Method waits when element with class $className become visible
+     *
+     * @param string $selector
+     *            Selector
+     * @param string $ErrorMessage
+     *            Error message
+     */
+    protected function waitForVisibilityBySelector(string $selector, string $ErrorMessage = ''): void
+    {
+        try {
+            $Element = \Facebook\WebDriver\WebDriverBy::cssSelector($selector);
+            $condition = \Facebook\WebDriver\WebDriverExpectedCondition::visibilityOfElementLocated($Element);
+            $this->Driver->wait()->until($condition);
+
+            $this->addToAssertionCount(1);
+        } catch (\Facebook\WebDriver\Exception\NoSuchElementException $e) {
+            $this->fail($ErrorMessage);
+        }
+    }
+
+    /**
+     * Method waits when element with class $className become invisible
+     *
+     * @param string $selector
+     *            Selector
+     * @param string $ErrorMessage
+     *            Error message
+     */
+    protected function wait_for_invisibility_by_selector(string $selector, string $ErrorMessage = ''): void
+    {
+        try {
+            $Element = \Facebook\WebDriver\WebDriverBy::cssSelector($selector);
+            $condition = \Facebook\WebDriver\WebDriverExpectedCondition::invisibilityOfElementLocated($Element);
+            $this->Driver->wait()->until($condition);
+
+            $this->addToAssertionCount(1);
+        } catch (\Facebook\WebDriver\Exception\NoSuchElementException $e) {
+            $this->fail($ErrorMessage);
+        }
+    }
+
+    /**
+     * Method clicks on element
+     *
+     * @param string $selector
+     *            Selector of the clicking element
+     */
+    protected function clickElement(string $selector): void
+    {
+        $this->waitForVisibilityBySelector($selector, 'Element was not shown');
+
+        $Element = $this->Driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector($selector));
+        $Element->click();
+    }
+
+    /**
+     * Method returns true if the test was lanched for mobile device
+     *
+     * @return bool true if the test was lanched for mobile device, false otherwise
+     */
+    protected function phone(): bool
+    {
+        global $argv;
+
+        return (in_array('iphonex', $argv));
+    }
+
+    /**
+     * Method returns true if the test was lanched for tablet device
+     *
+     * @return bool true if the test was lanched for tablet device, false otherwise
+     */
+    protected function tablet(): bool
+    {
+        global $argv;
+
+        return (in_array('ipad', $argv));
+    }
+
+    /**
+     * Method returns true if the test was launched for desktop
+     *
+     * @return bool true if the test was launched for desktop, false otherwise
+     */
+    protected function desktop(): bool
+    {
+        return ($this->phone() === false && $this->tablet() === false);
+    }
+
+    /**
+     * Method send data to the element with the specified selector
+     *
+     * @param string $selector
+     *            Element's selector
+     * @param string $Value
+     *            Value to be inputed
+     */
+    protected function input_in(string $selector, string $Value): void
+    {
+        $Element = $this->Driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector($selector));
+
+        $Element->click();
+        $Element->sendKeys($Value);
+    }
+
+    /**
+     * Waiting for page will be reloaded
+     *
+     * @param callable $Reloader
+     */
+    protected function wait_for_page_reload(callable $Reloader): void
+    {
+        $id = $this->Driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector('html'))->getID();
+
+        call_user_func($Reloader);
+
+        $this->Driver->wait()->until(
+            function () use ($id) {
+                if ($id != $this->Driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector('html'))
+                    ->getID()) {
+                    return (true);
+                }
+            });
+    }
+
+    /**
+     * Method clears input field
+     *
+     * @param string $selector
+     *            Selector of the input field
+     */
+    protected function clear_input(string $selector): void
+    {
+        $Input = $this->Driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector($selector));
+        $Input->clear();
+    }
+
+    /**
+     * Method asserts class name for the element
+     *
+     * @param string $selector
+     *            Selector of the element
+     * @param string $className
+     *            Class name
+     */
+    protected function assertHasClass(string $selector, string $className): void
+    {
+        $this->waitForVisibilityBySelector($selector . '.' . $className);
+    }
+
+    /**
+     * Scrolling to element
+     *
+     * @param string $selector
+     */
+    protected function scrollToElement(string $selector): void
+    {
+        $element = $this->Driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector($selector));
+        $action = new \Facebook\WebDriver\Interactions\WebDriverActions($this->Driver);
+        $action->moveToElement($element);
+        $action->perform();
+    }
+
+    /**
+     * Method returns true if the parameter 'prod' was passed through command line
+     *
+     * @return bool
+     */
+    protected function isProd(): bool
+    {
+        global $argv;
+
+        return in_array('prod', $argv);
+    }
+
+    /**
+     * Method returns true if the element is visible
+     *
+     * @param string $selector
+     *            element's selector
+     * @return bool true if visible, false otherwise
+     */
+    protected function isVisible(string $selector): bool
+    {
+        $element = $this->Driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector($selector));
+
+        return $element->isDisplayed();
+    }
+
+    /**
+     * Method returns true if the element exists
+     *
+     * @param string $selector
+     *            selector of the testing element
+     * @return bool true if the element exists, false otherwise
+     */
+    protected function elementExists(string $selector): bool
+    {
+        try {
+            $this->Driver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector($selector));
+
+            return (true);
+        } catch (\Facebook\WebDriver\Exception\NoSuchElementException $e) {
+            return (false);
+        }
+    }
+}
